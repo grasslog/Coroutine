8f854e4d921d4257d52273a1a27c04f04d4950ee
before25tofree <2869821598@qq.com>
2019-05-20 16:52:01 +0800

    Set up coroutine.h header file

---
+++ coroutine.h
@@ -0,0 +1,21 @@
+#pragma
+
+#define COROUTINE_DEAD 0
+#define COROUTINE_READY 1
+#define COROUTINE_RUNNING 2
+#define COROUTINE_SUSPEND 3
+
+struct schedule;
+
+typedef void (*coroutine_func)(struct schedule *, void *ud);
+
+struct schedule * coroutine_open(void);
+void coroutine_close(struct schedule *);
+
+int coroutine_new(struct schedule *, coroutine_func, void *ud);
+void coroutine_resume(struct schedule *, int fd);
+int coroutine_status(struct schedule *, int fd);
+int coroutine_running(struct schedule *);
+void coroutine_yield(struct schedule *);
+
+#endif
